274ed4e3ab76a0bf48ee82fe3867fe11c5ee802a
蒋龙 <kornan@sina.com>
2016-02-03 10:38:03 +0800

    test22

--- app/src/main/java/net/kornan/animview/MainActivity.java
+++ app/src/main/java/net/kornan/animview/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        number = (TextView) findViewById(R.id.number);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         DataNumber datas = new DataNumber(""+88,""+0);
