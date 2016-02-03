d6561d7766ecbfccfeaffa3f257034aba15dd711
蒋龙 <kornan@sina.com>
2016-02-03 16:27:51 +0800

    添加app

--- app/src/main/java/net/kornan/animview/MainActivity.java
+++ app/src/main/java/net/kornan/animview/MainActivity.java
@@ -3,22 +3,36 @@ package net.kornan.animview;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import net.kornan.animview.beans.DataNumber;
 import net.kornan.animview.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-//    TextView number;
+    //    TextView number;
     ActivityMainBinding binding;
+    DataNumber datas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        DataNumber datas = new DataNumber(""+88,""+0);
+        datas = new DataNumber("" + 88, "" + 0);
         binding.setData(datas);
-        datas.setStartNum("90");
+
+    }
+
+    public void onClickView(View v) {
+        switch (v.getId()) {
+            case R.id.number:
+                datas.setStartNum("90");
+                Log.i("number", datas.getStartNum());
+
+                break;
+        }
     }
 }
--- app/src/main/java/net/kornan/animview/beans/DataNumber.java
+++ app/src/main/java/net/kornan/animview/beans/DataNumber.java
@@ -1,9 +1,12 @@
 package net.kornan.animview.beans;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by KORNAN on 2016/2/1.
  */
-public class DataNumber {
+public class DataNumber extends BaseObservable {
     private String startNum;
     private String endNum;
 
@@ -12,19 +15,24 @@ public class DataNumber {
         this.endNum = lastName;
     }
 
+    @Bindable
     public String getStartNum() {
         return startNum;
     }
 
+
     public void setStartNum(String startNum) {
         this.startNum = startNum;
+        notifyPropertyChanged(net.kornan.animview.BR.startNum);
     }
 
+    @Bindable
     public String getEndNum() {
         return endNum;
     }
 
     public void setEndNum(String endNum) {
         this.endNum = endNum;
+        notifyPropertyChanged(net.kornan.animview.BR.endNum);
     }
 }
